b753868f7c3225292183d1e8fb14a7a292260c62
Andrei Miron <victormiron.home@gmail.com>
2024-02-10 15:10:35 -0500

    trying json

---
+++ trying_json.py
@@ -0,0 +1,17 @@
+import json
+
+shopping_list = [
+    ['apples', 3],
+    ['bananas', 2],
+    ['prunes', 5],
+]
+
+with open('trying.json', 'w', encoding='utf-8') as shp_lst:
+    json.dump(shopping_list, shp_lst)
+
+with open('trying.json', 'r', encoding='utf-8') as shp_lst:
+    data = json.load(shp_lst)
+print(data)
+print(data[1])
+
+
